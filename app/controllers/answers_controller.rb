f4b2391dc03655b3520700f6970b30f623f004e2
ryouta9121 <nyagoro33@gmail.com>
2022-01-30 17:30:15 +0900

    answer edit

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,8 +1,22 @@
 class AnswersController < ApplicationController
 
   def create
+    @question = Question.find(params[:question_id])
+    @answer = Answer.new
+    if @answer.update(answer_params)
+      redirect_to question_path(@question), notice: 'Success!'
+    else
+      redirect_to question_path(@question), alert: 'Invalid!'
+    end
   end
 
   def edit
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.find(params[:id])
   end
+
+  private
+    def answer_params
+      params.require(:answer).permit(:content, :name, :question_id)
+    end
 end
