67148bd778c5024d7531924fb44165d72a925bd7
ryouta9121 <nyagoro33@gmail.com>
2022-01-31 23:56:44 +0900

    save file

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,4 @@
 class AnswersController < ApplicationController
-
   def create
     @question = Question.find(params[:question_id])
     @answer = Answer.new
@@ -13,6 +12,25 @@ class AnswersController < ApplicationController
   def edit
     @question = Question.find(params[:question_id])
     @answer = @question.answers.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.find(params[:id])
+    if @answer.update(answer_params)
+      redirect_to question_path(@question), notice: 'Success!'
+    else
+      flash[:alert] = 'Invalid'
+      render :edit
+    end
+
+  end
+  
+  def destroy
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.find(params[:id])
+    @answer.destroy
+    redirect_to question_path(@question), notice: 'Deleted!'
   end
 
   private
